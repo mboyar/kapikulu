2f14174d834dbe5f9ba595d351d9b8f213c4804b
murat boyar <muratboyar@gmail.com>
2016-05-02 23:41:17 +0300

    take photo and send

--- test-photo.py
+++ test-photo.py
@@ -2,6 +2,43 @@ import tweepy
 import os
 import sys
 import config
+import cv2
+
+## cam stuff, thx to codeplasma.com
+
+# Camera 0 is the integrated web cam on my netbook
+camera_port = 0
+
+#Number of frames to throw away while the camera adjusts to light levels
+ramp_frames = 30
+
+# Now we can initialize the camera capture object with the cv2.VideoCapture class.
+# All it needs is the index to a camera port.
+camera = cv2.VideoCapture(camera_port)
+
+# Captures a single image from the camera and returns it in PIL format
+def get_image():
+ # read is the easiest way to get a full image out of a VideoCapture object.
+ retval, im = camera.read()
+ return im
+
+# Ramp the camera - these frames will be discarded and are only used to allow v4l2
+# to adjust light levels, if necessary
+for i in xrange(ramp_frames):
+ temp = get_image()
+print("Taking image...")
+# Take the actual image we want to keep
+camera_capture = get_image()
+# A nice feature of the imwrite method is that it will automatically choose the
+# correct format based on the file extension you provide. Convenient!
+cv2.imwrite(config.mediafile_path, camera_capture)
+
+# You'll want to release the camera, otherwise you won't be able to create a new
+# capture object until your script exits
+del(camera)
+
+
+## tweet stuffs
 
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.secure = True
@@ -9,8 +46,4 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
-#api.update_with_media("/home/murat/Pictures/20160418_220038.jpg")
-
-api.send_direct_message(screen_name="mboyar_ei", text="selam murat, ben ev")
-
-
+api.update_with_media(config.mediafile_path)
